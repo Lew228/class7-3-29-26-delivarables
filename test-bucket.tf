845fbee0caf3fb42e4489f1e919b8cff6f017084
Lew228 <Shawnmosby@yahoo.com>
2026-03-29 01:00:09 -0600

    Update terraform backent to new s3

--- test-bucket.tf
+++ test-bucket.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "terraform-state-aaronmcd"                 # Name of the S3 bucket
-    key     = "jenkins-test-031726.tfstate"        # The name of the state file in the bucket
-    region  = "us-east-2"                          # Use a variable for the region
+    bucket  = "shawn-terraform-state-2026"                 # Name of the S3 bucket
+    key     = "jenkins-s3-test/terraform.tfstate"        # The name of the state file in the bucket
+    region  = "us-east-1"                          # Use a variable for the region
     encrypt = true                                 # Enable server-side encryption (optional but recommended)
   } 
 }
